0b65afafddf46b876a735dcb0d93d7423d0f6ddf
Administrador <Administrador@10.10.4.1>
2019-10-01 16:08:09 -0300

    esse foi

--- Sacole/src/controlador/ControladorSacole.java
+++ Sacole/src/controlador/ControladorSacole.java
@@ -5,6 +5,7 @@
  * and open the template in the editor.
  */
 package controlador;
+
 import dao.DaoSacole;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -23,22 +24,26 @@ import javax.swing.table.DefaultTableModel;
  */
 public class ControladorSacole {
 
-    public static void inserir(ManutencaoSacole man){
+    public static void inserir(ManutencaoSacole man) {
         Sacole objeto = new Sacole();
         objeto.setSabor(man.jtfSabor.getText());
         objeto.setData_validade(LocalDate.parse(man.jtfData_validade.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         objeto.setPreco(Double.parseDouble(man.jtfPreco.getText()));
         objeto.setNr_serie(Integer.parseInt(man.jtfNr_serie.getText()));
-        
+
         boolean resultado = DaoSacole.inserir(objeto);
         if (resultado) {
             JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+            if (man.listagem != null) {
+                atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+            }
+            man.dispose();
         } else {
             JOptionPane.showMessageDialog(null, "Erro!");
         }
-}
+    }
 
-    public static void alterar(ManutencaoSacole man){
+    public static void alterar(ManutencaoSacole man) {
         Sacole objeto = new Sacole();
         //definir todos os atributos
         objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
@@ -46,32 +51,35 @@ public class ControladorSacole {
         objeto.setData_validade(LocalDate.parse(man.jtfData_validade.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         objeto.setPreco(Double.parseDouble(man.jtfPreco.getText()));
         objeto.setNr_serie(Integer.parseInt(man.jtfNr_serie.getText()));
-        
+
         boolean resultado = DaoSacole.alterar(objeto);
         if (resultado) {
             JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+            if (man.listagem != null) {
+                atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+            }
+            man.dispose();
         } else {
             JOptionPane.showMessageDialog(null, "Erro!");
         }
     }
 
-    public static void excluir(ManutencaoSacole man){
+    public static void excluir(ManutencaoSacole man) {
         Sacole objeto = new Sacole();
         objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
-        
+
         boolean resultado = DaoSacole.excluir(objeto);
         if (resultado) {
             JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
-        if (man.listagem != null) {
-        atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
-          }
-        man.dispose();//fechar a tela da manutenção    
+            if (man.listagem != null) {
+                atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+            }
+            man.dispose();//fechar a tela da manutenção    
         } else {
             JOptionPane.showMessageDialog(null, "Erro!");
         }
     }
-    
-    
+
     public static void atualizarTabela(JTable tabela) {
         DefaultTableModel modelo = new DefaultTableModel();
         //definindo o cabeçalho da tabela
@@ -83,7 +91,7 @@ public class ControladorSacole {
         List<Sacole> resultados = DaoSacole.consultar();
         for (Sacole objeto : resultados) {
             Vector linha = new Vector();
-            
+
             //definindo o conteúdo da tabela
             linha.add(objeto.getCodigo());
             linha.add(objeto.getSabor());
@@ -94,8 +102,8 @@ public class ControladorSacole {
         }
         tabela.setModel(modelo);
     }
-    
-    public static void atualizaCampos(ManutencaoSacole man, int pk){ 
+
+    public static void atualizaCampos(ManutencaoSacole man, int pk) {
         Sacole objeto = DaoSacole.consultar(pk);
         //Definindo os valores do campo na tela (um para cada atributo/campo)
         man.jtfCodigo.setText(objeto.getCodigo().toString());
@@ -103,9 +111,9 @@ public class ControladorSacole {
         man.jtfData_validade.setText(objeto.getData_validade().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         man.jtfPreco.setText(objeto.getPreco().toString());
         man.jtfNr_serie.setText(objeto.getNr_serie().toString());
-        
+
         man.jtfCodigo.setEnabled(false); //desabilitando o campo código
         man.btnAdicionar.setEnabled(false); //desabilitando o botão adicionar
     }
-    
+
 }
